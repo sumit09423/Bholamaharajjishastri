110b580319536da9172a6aa2e064145e9fd088de
Sumit Chauhan <85306802+sumit09423@users.noreply.github.com>
2026-02-21 10:50:52 +0530

    Add Google Tag Manager script to index.html and index.php for tracking

--- index.php
+++ index.php
@@ -49,6 +49,15 @@ if(isset($_POST['submit'])) {
 <html lang="en">
   
   <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-10836639423"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'AW-10836639423');
+    </script>
     <!-- Required meta tags -->
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
